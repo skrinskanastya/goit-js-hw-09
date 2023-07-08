9994cb4e8b5c02cd1f9f55acea4600d9c07cb3fa
skrinskanastya <101664125+skrinskanastya@users.noreply.github.com>
2023-07-08 10:28:19 +0000

    Deploying to gh-pages from @ skrinskanastya/goit-js-hw-09@46048b921132a5b5affd60df9e7a7f120ec0ce10 🚀

---
+++ 01-color-switcher.0f11f862.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;function o(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.0f11f862.js.map
---
+++ 01-color-switcher.20a415a8.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(a,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.20a415a8.js.map
